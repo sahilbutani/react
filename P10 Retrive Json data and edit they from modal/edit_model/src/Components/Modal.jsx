d5288158933374c02cb1c82f47452da4dfea81db
Sahil Butani <m.s.butani777@gmail.com>
2025-01-16 15:20:57 +0530

    Update Modal.jsx

--- P10 Retrive Json data and edit they from modal/edit_model/src/Components/Modal.jsx
+++ P10 Retrive Json data and edit they from modal/edit_model/src/Components/Modal.jsx
@@ -42,8 +42,8 @@ const Modal = ({ open, setOpen, emp, setOriginal }) => {
       id="default-modal"
       tabIndex="-1"
       aria-hidden="true"
-      className={`${
-        open === false ? "hidden" : ""
+      className={
+        `${open === false ? "hidden" : ""
       } overflow-y-auto overflow-x-hidden fixed top-50 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
     >
       {/* jo open=false thse etle k modal hidden thai jse  */}
